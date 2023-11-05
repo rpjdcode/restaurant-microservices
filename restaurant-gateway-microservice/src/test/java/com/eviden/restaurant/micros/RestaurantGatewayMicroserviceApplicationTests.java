69be81ae87336f79b7905dffc057e1eecf7b2caf
rpjcode <69083318+rpjdcode@users.noreply.github.com>
2023-11-05 02:07:44 +0000

    Adición de microservicio de puerta de enlace

---
+++ restaurant-gateway-microservice/src/test/java/com/eviden/restaurant/micros/RestaurantGatewayMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eviden.restaurant.micros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantGatewayMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
